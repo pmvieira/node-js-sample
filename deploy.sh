13eb7684abe730e2f06e92f7c4397aaaf79b9524
Pedro Vieira <pedrovieira@quillcontent.com>
2015-10-14 16:21:00 +0100

    adding debug

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,17 @@ set -e
 # Deploy image to private GCS-backed registry
 gcloud docker push $GCR_HOST/$IMAGE:$CIRCLE_SHA1
 
+echo "debug1"
+
 # Update Kubernetes replicationController
 envsubst < gke/app-controller.json.template > app-controller.json
+
+echo "debug2"
 #$KUBE_CMD -c app-controller.json \
 #    update replicationControllers/app-controller
 kubectl replace -f app-controller.json
 
+echo "debug3"
 # Roll over Kubernetes pods
 #$KUBE_CMD rollingupdate app-controller
 kubectl rolling-update $IMAGE:$CIRCLE_SHA1 -f app-controller.json
