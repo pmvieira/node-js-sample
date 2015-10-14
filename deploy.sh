407a59409274b19c0ced08e5d5b0ad277d7fa9aa
Pedro Vieira <pedrovieira@quillcontent.com>
2015-10-14 16:09:35 +0100

    using circle SHA instead of hardcoded tag

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 # KUBE_CMD=${KUBERNETES_ROOT:-~/kubernetes}/cluster/kubecfg.sh
 
 # Deploy image to private GCS-backed registry
-gcloud docker push $GCR_HOST/$IMAGE:$TAG
+gcloud docker push $GCR_HOST/$IMAGE:$CIRCLE_SHA1
 
 # Update Kubernetes replicationController
 envsubst < gke/app-controller.json.template > app-controller.json
@@ -17,4 +17,4 @@ kubectl replace -f app-controller.json
 
 # Roll over Kubernetes pods
 #$KUBE_CMD rollingupdate app-controller
-kubectl rolling-update $IMAGE:TAG -f app-controller.json
+kubectl rolling-update $IMAGE:$CIRCLE_SHA1 -f app-controller.json
